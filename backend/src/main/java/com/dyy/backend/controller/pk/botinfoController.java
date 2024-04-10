ef8b5ae7a747250b5fddd5e6943df3fc9475758b
宇扬 代 <1148966780@qq.com>
2024-04-10 17:14:58 +0800

    项目框架

--- backend/src/main/java/com/dyy/backend/controller/pk/botinfoController.java
+++ backend/src/main/java/com/dyy/backend/controller/pk/botinfoController.java
@@ -3,11 +3,17 @@ package com.dyy.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class botinfoController {
     @RequestMapping("/getbotinfo/")
-    public String getbotinfo() {
-        return "hhhh";
+    public Map<String, String> getbotinfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1200");
+        return bot1;
     }
 }
