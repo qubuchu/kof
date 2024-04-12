a70f4e3c6e7d80bb28c2717ad294e58511871ec8
宇扬 代 <1148966780@qq.com>
2024-04-12 17:07:39 +0800

    配置mysql和安全登录

---
+++ backend/src/main/java/com/dyy/backend/pojo/User.java
@@ -0,0 +1,14 @@
+package com.dyy.backend.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+}
